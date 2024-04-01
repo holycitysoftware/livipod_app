f795aed67044428dc2b9ba7a83995068f9555bb3
gfb-47 <gabrieelfbrito@gmail.com>
2024-04-01 14:14:33 +0200

    First widgets of identify persona page

--- lib/views/registration/identify_persona_page.dart
+++ lib/views/registration/identify_persona_page.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../../components/components.dart';
+import '../../utils/strings.dart';
+
 class IdentifyPersonaPage extends StatelessWidget {
   const IdentifyPersonaPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          LiviTextStyles.interMedium24(Strings.howOftenDoYouNeedAssistance),
+          LiviTextStyles.interRegular16(Strings.selectTheMostAccurateAnswer),
+        ],
+      ),
+    );
   }
 }
