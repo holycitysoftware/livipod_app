f8996af51ca7a8f9477cb9c0fd1c93966e237e96
gfb-47 <gabrieelfbrito@gmail.com>
2024-06-11 11:33:33 +0200

    Remove all code to show notifications at first moment

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:permission_handler/permission_handler.dart';
 // import 'package:livipod_app/controllers/communication_controller.dart';
 import 'package:provider/provider.dart';
 import 'controllers/controllers.dart';
@@ -141,79 +139,80 @@ class _MyAppState extends State<MyApp> {
   final LiviPodService _liviPodController = LiviPodService();
   final AuthController _authController = AuthController();
   final MessagingController _messagingController = MessagingController();
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
-  var initializationSettingsAndroid = AndroidInitializationSettings(
-      'res_app_icon'); // <- default icon name is @mipmap/ic_launcher
-  //  var initializationSettingsIOS = IOSInitializationSettings(onDidReceiveLocalNotification: onDidReceiveLocalNotification);
-  AndroidNotificationDetails androidNotificationDetails =
-      AndroidNotificationDetails(
-    'your channel id',
-    'your channel name',
-    channelDescription: 'your channel description',
-    importance: Importance.max,
-    priority: Priority.high,
-    colorized: true,
-    color: LiviThemes.colors.brand500,
-    ticker: 'ticker',
-  );
-  late final DarwinInitializationSettings initializationSettingsDarwin;
 
-  Future<void> onDidReceiveLocalNotification(
-      int id, String? title, String? body, String? payload) async {
-    // display a dialog with the notification details, tap ok to go to another page
-    showDialog(
-      context: context,
-      builder: (BuildContext context) => CupertinoAlertDialog(
-        title: Text(title ?? ''),
-        content: Text(body ?? ''),
-        actions: [
-          CupertinoDialogAction(
-            isDefaultAction: true,
-            child: Text('Ok'),
-            onPressed: () async {
-              Navigator.of(context, rootNavigator: true).pop();
-              await Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => SmsFlowPage(),
-                ),
-              );
-            },
-          )
-        ],
-      ),
-    );
-  }
+  // FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  //     FlutterLocalNotificationsPlugin();
+  // var initializationSettingsAndroid = AndroidInitializationSettings(
+  //     'res_app_icon'); // <- default icon name is @mipmap/ic_launcher
+  // //  var initializationSettingsIOS = IOSInitializationSettings(onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+  // AndroidNotificationDetails androidNotificationDetails =
+  //     AndroidNotificationDetails(
+  //   'your channel id',
+  //   'your channel name',
+  //   channelDescription: 'your channel description',
+  //   importance: Importance.max,
+  //   priority: Priority.high,
+  //   colorized: true,
+  //   color: LiviThemes.colors.brand500,
+  //   ticker: 'ticker',
+  // );
+  // late final DarwinInitializationSettings initializationSettingsDarwin;
 
-  late NotificationDetails notificationDetails;
+  // Future<void> onDidReceiveLocalNotification(
+  //     int id, String? title, String? body, String? payload) async {
+  //   // display a dialog with the notification details, tap ok to go to another page
+  //   showDialog(
+  //     context: context,
+  //     builder: (BuildContext context) => CupertinoAlertDialog(
+  //       title: Text(title ?? ''),
+  //       content: Text(body ?? ''),
+  //       actions: [
+  //         CupertinoDialogAction(
+  //           isDefaultAction: true,
+  //           child: Text('Ok'),
+  //           onPressed: () async {
+  //             Navigator.of(context, rootNavigator: true).pop();
+  //             await Navigator.push(
+  //               context,
+  //               MaterialPageRoute(
+  //                 builder: (context) => SmsFlowPage(),
+  //               ),
+  //             );
+  //           },
+  //         )
+  //       ],
+  //     ),
+  //   );
+  // }
+
+  // late NotificationDetails notificationDetails;
 
   @override
   void initState() {
     _devicesController = BleController(liviPodController: _liviPodController);
     // _scheduleController =
     //     ScheduleController(liviPodController: _liviPodController);
-    notificationDetails =
-        NotificationDetails(android: androidNotificationDetails);
-    initializationSettingsDarwin = DarwinInitializationSettings(
-        onDidReceiveLocalNotification: onDidReceiveLocalNotification);
-    doAsyncStuff();
+    // notificationDetails =
+    //     NotificationDetails(android: androidNotificationDetails);
+    // initializationSettingsDarwin = DarwinInitializationSettings(
+    //     onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+    // doAsyncStuff();
     super.initState();
   }
 
-  Future<void> doAsyncStuff() async {
-    var initializationSettings = InitializationSettings(
-        android: initializationSettingsAndroid,
-        iOS: initializationSettingsDarwin);
+  // Future<void> doAsyncStuff() async {
+  //   var initializationSettings = InitializationSettings(
+  //       android: initializationSettingsAndroid,
+  //       iOS: initializationSettingsDarwin);
 
-    flutterLocalNotificationsPlugin.initialize(
-      initializationSettings,
-    );
+  //   flutterLocalNotificationsPlugin.initialize(
+  //     initializationSettings,
+  //   );
 
-    await flutterLocalNotificationsPlugin.show(2, 'Baclofen 10 mg Tablet',
-        'Time to take your 9:40 AM medications', notificationDetails,
-        payload: 'item x');
-  }
+  //   await flutterLocalNotificationsPlugin.show(2, 'Baclofen 10 mg Tablet',
+  //       'Time to take your 9:40 AM medications', notificationDetails,
+  //       payload: 'item x');
+  // }
 
   @override
   Widget build(BuildContext context) {
