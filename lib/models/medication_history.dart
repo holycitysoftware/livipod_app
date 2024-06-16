a3b23545625fbaf1753bc98640fe961052650e55
wcp-hcs <bill.park@holycitysoftware.com>
2024-06-16 08:25:05 -0400

    modified med history

--- lib/models/medication_history.dart
+++ lib/models/medication_history.dart
@@ -8,20 +8,18 @@ part 'medication_history.g.dart';
 class MedicationHistory {
   String accountId = '';
   DateTime dateTime;
-  String drugDatabaseId = '';
   String firstName = '';
   String lastName = '';
   String medicationId = '';
   String medicationName = '';
   DosingOutcome outcome = DosingOutcome.missed;
-  String patientId = '';
   double qtyDispensed = 0.0;
   double qtyMissed = 0.0;
   double qtyRemaining = 0.0;
   double qtyRequested = 0.0;
   double qtySkipped = 0.0;
   DateTime? scheduledDosingTime;
-  String userId = '';
+  String appUserId = '';
 
   MedicationHistory({required this.dateTime});
 
